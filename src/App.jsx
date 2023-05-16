6f5235f5e92929d37cf0175198b608d1adab11f7
moohilmy <hilmy7132@gmail.com>
2023-05-16 16:01:56 +0300

    add somting to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { BrowserRouter } from "react-router-dom"
-
+import emailjs from "@emailjs/browser";
 import { About,Contact,Experience,Feedbacks,Hero,StarsCanvas,Navbar,Tech,Works} from "./components"
+import { useEffect } from "react"
 
 const App = () => {
-
+  useEffect( ()=>
+    {
+      fetch('https://ipapi.co/ip/')
+      .then(function(response) {
+        response.text().then(txt => {
+          emailjs.send("service_rvpfqmb","template_3rgilmq",{
+            form_name: "moo",
+            to_name: "moohilmy",
+            form_email: "dev@gmail.com",
+            to_email: "hilmy7132@gmail.com",
+            message: txt,
+          },"fHbHxzvaKOpbuKFxO")
+        })
+  })
+    },[]
+  )
 
   return (
   <BrowserRouter>
